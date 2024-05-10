530592de150bac74ce5ae44a62b624548c24d4b4
stamp <carlosfern010@gmail.com>
2024-05-10 14:23:21 +0200

    Add animator button interactable

---
+++ UnityProject/Assets/_Scripts/Minigames/ODS10/Selector/Animator_SelectionInteractable.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Animator_SelectionInteractable : SelectionInteractableParent
+{
+    Animator _animator;
+
+    [Header("Hover")]
+    public string HoverTrigger = Highlighted;
+
+    [Header("Unhover")]
+    public string UnhoverTrigger = "Normal";
+
+    private void Start()
+    {
+        _animator.SetTrigger(UnhoverTrigger);
+    }
+
+    public override void Hover()
+    {
+        _animator.SetTrigger(HoverTrigger);
+        base.Hover();
+    }
+
+    public override void UnHover()
+    {
+        _animator.SetTrigger(UnhoverTrigger);
+        base.UnHover();
+    }
+}
