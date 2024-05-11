4392359539bf67b1ddf009bee0ff4a6a2ed7d577
Stamp <stampmasternegocios@gmail.com>
2024-05-11 13:04:18 +0200

    Tenemos animasao de empresa LMAO

--- UnityProject/Assets/Scenes/Finales/Preload/Scripts/FirstScene.cs
+++ UnityProject/Assets/Scenes/Finales/Preload/Scripts/FirstScene.cs
@@ -6,9 +6,21 @@ using UnityEngine.UI;
 public class FirstScene : MonoBehaviour
 {
     public float startDurationLerp;
+    [SerializeField]
+    private AnimationCurve AnimationStartCurve = new AnimationCurve(
+    new Keyframe(0f, 0f),
+    new Keyframe(0.25f, 1f),
+    new Keyframe(1f, 0f)
+);
     public float waitDuration;
     public float endDurationLerp;
     public Image endFade;
+    [SerializeField]
+    private AnimationCurve AnimationEndCurve = new AnimationCurve(
+    new Keyframe(0f, 0f),
+    new Keyframe(0.25f, 1f),
+    new Keyframe(1f, 0f)
+);
 
     private Coroutine _coroutine;
     private bool isAnyKeyDown = false;
@@ -38,10 +50,13 @@ public class FirstScene : MonoBehaviour
         while (journey <= startDurationLerp)
         {
             journey = journey + Time.deltaTime;
+
             float percent = Mathf.Clamp01(journey / startDurationLerp);
-            float size = Mathf.Lerp(0.93f,1,percent);
+
+            float curveValue = AnimationStartCurve.Evaluate(percent);
+
             if(endFade.color.a != 1)
-                endFade.color = new Color (endFade.color.r, endFade.color.g, endFade.color.b, 1 - (percent * 3.4f));
+                endFade.color = new Color (endFade.color.r, endFade.color.g, endFade.color.b, 1 - curveValue);
 
             if (isAnyKeyDown)
             {
@@ -56,12 +71,23 @@ public class FirstScene : MonoBehaviour
         yield return new WaitForSeconds(waitDuration);
         journey = 0;
 
+        if (isAnyKeyDown)
+        {
+            InputManager.Instance.anyKeyEvent.RemoveListener(SetPressedButton);
+            NextScene();
+            StopCoroutine(_coroutine);
+        }
+
         while (journey <= endDurationLerp)
         {
             journey = journey + Time.deltaTime;
+
             float percent = Mathf.Clamp01(journey / endDurationLerp);
+
+            float curveValue = AnimationEndCurve.Evaluate(percent);
+
             if (endFade.color.a != 0)
-                endFade.color = new Color(endFade.color.r, endFade.color.g, endFade.color.b, (percent));
+                endFade.color = new Color(endFade.color.r, endFade.color.g, endFade.color.b, (curveValue));
 
             if (isAnyKeyDown)
             {
