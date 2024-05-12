6596927432fdd87a4a02d984d8133e5cb1023be6
Francisco Miguel Sanz Gatzkiewicz <franmi.sanz.gatz@gmail.com>
2024-05-12 20:12:02 +0200

    Score and Win/Lose Implemented

--- UnityProject/Assets/_Scripts/Minigames/ODS14/ODS14Manager.cs
+++ UnityProject/Assets/_Scripts/Minigames/ODS14/ODS14Manager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 public class ODS14Manager : MinigameParent
 {
@@ -10,8 +11,11 @@ public class ODS14Manager : MinigameParent
     public LivesManager playerLives;
 
     public UnityEvent reduceLife;
+    public UnityEvent garbageHit;
     
     private int _currentLives;
+    [SerializeField] private int _garbageLeft;
+    private List<GameObject> _floatingGarbage;
 
     public bool canMove;
     
@@ -27,13 +31,17 @@ public class ODS14Manager : MinigameParent
         base.personalStart();
         timer.PreSetTimmer();
         _currentLives = playerLives.playerLivesSprites.Length;
+        
         reduceLife.AddListener(DecreaseLives);
+        garbageHit.AddListener(GarbageHit);
     }
     
     protected override void OnGameStart()
     {
         base.OnGameStart();
         timer.SetTimer();
+        
+        InitializeGarbageLeft();
         EnableMovement();
     }
     
@@ -41,11 +49,22 @@ public class ODS14Manager : MinigameParent
     {
         base.OnGameFinish();
         DisableMovement();
+        timer.PauseTimer();
     }
 
     public void AnimalHit()
     {
         playerLives.updateLives.Invoke();
+    }
+
+    private void GarbageHit()
+    {
+        _garbageLeft--;
+        if (_garbageLeft <= 0)
+        {
+            _garbageLeft = 0;
+            OnGameFinish();
+        }
     }
 
     public void DisableMovement()
@@ -57,6 +76,11 @@ public class ODS14Manager : MinigameParent
         canMove = true;
     }
 
+    private void InitializeGarbageLeft()
+    {
+        _garbageLeft = FindObjectsOfType<FloatingGarbage>().Length;
+    }
+
     private void DecreaseLives()
     {
         _currentLives--;
@@ -64,7 +88,33 @@ public class ODS14Manager : MinigameParent
         {
             _currentLives = 0;
             OnGameFinish();
+        }
+    }
+
+    public override void SetResult()
+    {
+        Debug.Log("Se hace tranquilo");
+        Debug.Log(Score);
+
+        RankImage.sprite = RankData.timerImageArray[MinigameData.CheckPointsState(Score)].sprite;
+
+        _ScoreText.ChangeText(timer.GetTimeInSeconds());
+
+        int minutos = Mathf.FloorToInt(MinigameData.maxPoints / 60);
+        int segundos = Mathf.FloorToInt(MinigameData.maxPoints % 60);
+
+        _txHighScore.text = "High: " + string.Format("{0:00}:{1:00}", minutos, segundos/*, milisegundos*/);
+    }
+
+    public override void SaveValue()
+    {
+        if (_garbageLeft != 0)
+        {
+            SaveValue(-1);
+            Score = -1;
             return;
         }
+        Score = timer.GetRealTime();
+        SaveValue(Score);
     }
 }
