631b86f73ede0a502e521e678c97d0a796de0d1f
Stamp <stampmasternegocios@gmail.com>
2024-05-13 17:47:43 +0200

    pos ahora hay estrellitas que te dien el estado del minijuego

---
+++ UnityProject/Assets/_Scripts/HUD/InGame/StarScriptableObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StarImage", menuName = "ScriptableObjects/Utility/StarImage", order = 6)]
+public class StarScriptableObject : ScriptableObject
+{
+    public Sprite On;
+    public Sprite Off;
+}
