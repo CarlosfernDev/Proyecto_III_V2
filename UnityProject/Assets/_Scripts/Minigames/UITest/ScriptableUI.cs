8eaa6070602ff44c0a1f404e1f0502179851483e
Stamp <stampmasternegocios@gmail.com>
2024-05-23 21:08:04 +0200

    Add cosas para almacenar mas datos

--- UnityProject/Assets/_Scripts/Minigames/UITest/ScriptableUI.cs
+++ UnityProject/Assets/_Scripts/Minigames/UITest/ScriptableUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [CreateAssetMenu(fileName = "UIdata", menuName = "ScriptableObjects/UIdata", order = 1)]
 public class ScriptableUI : ScriptableObject
@@ -11,4 +12,14 @@ public class ScriptableUI : ScriptableObject
     public Sprite UsarEquipable;
     public Sprite RotarPieza;
     public Sprite AnyKey;
+
+    public ScriptableUiComponent Test;
+}
+
+[Serializable]
+public class ScriptableUiComponent
+{
+    public Sprite Sprite1;
+    public Sprite Sprite2;
+    public AnimatorOverrideController Animator;
 }
