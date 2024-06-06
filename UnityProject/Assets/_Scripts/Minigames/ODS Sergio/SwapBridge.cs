d2f5ea730336e549f758786717a880b316dd22b1
Bo1x <alejandroboixperez@gmail.com>
2024-06-06 03:45:51 +0200

    asdasd

--- UnityProject/Assets/_Scripts/Minigames/ODS Sergio/SwapBridge.cs
+++ UnityProject/Assets/_Scripts/Minigames/ODS Sergio/SwapBridge.cs
@@ -68,6 +68,7 @@ public class SwapBridge : LInteractableParent
         }
         GetComponent<BoxCollider>().isTrigger = true;
         StartCoroutine(SwapBridgeCoroutine());
+        base.Unhover();
     }
 
     private IEnumerator SwapBridgeCoroutine()
